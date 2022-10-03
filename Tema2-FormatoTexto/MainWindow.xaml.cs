dc84fb3f756bdcf15a687dc50d94de5f399b5b62
Jose Antonio Guerrero Vidal <90720873+joseantoniogv@users.noreply.github.com>
2022-10-03 09:03:55 +0200

    Add files via upload

---
+++ Tema2-FormatoTexto/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tema2_FormatoTexto
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            AzulButton.IsChecked = true;
+        }
+
+        private void NegritaCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            TituloTextBlock.FontWeight = FontWeights.Bold;
+        }
+
+        private void NegritaCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            TituloTextBlock.FontWeight = FontWeights.Normal;
+        }
+
+        private void CuadroTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            TituloTextBlock.Text = CuadroTextBox.Text;
+        }
+
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void CursivaCheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            TituloTextBlock.FontStyle = FontStyles.Italic;
+        }
+
+        private void CursivaCheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            TituloTextBlock.FontStyle = FontStyles.Normal;
+        }
+
+        private void AzulButton_Checked(object sender, RoutedEventArgs e)
+        {
+            TituloTextBlock.Foreground = new SolidColorBrush(Colors.Blue);
+        }
+
+        private void RojoButton_Checked(object sender, RoutedEventArgs e)
+        {
+            TituloTextBlock.Foreground = new SolidColorBrush(Colors.Red);
+
+        }
+
+        private void VerdeButton_Checked(object sender, RoutedEventArgs e)
+        {
+            TituloTextBlock.Foreground = new SolidColorBrush(Colors.Green);
+        }
+
+    }
+}
